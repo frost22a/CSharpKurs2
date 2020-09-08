8044a57d0a59ccbb8ba7fc29bd3f7044beee1c97
Przemek Mroz <frost22a@gmail.com>
2020-09-08 23:01:44 +0200

    create class Klient

---
+++ Kaczorek/Kaczorek.BL/Klient.cs
@@ -0,0 +1,47 @@
+﻿namespace Kaczorek.BL
+{
+    public class Klient
+    {
+        private string _nazwisko; //prywatne pole nazwisko - dostęp tylko z klasy, a spoza klasy przy pomocy akcesorów, za pomocą włąściwości:
+        
+        public string Nazwisko // właściwość pola _nazwisko || można wpisać modyfikator dostępu internal - wtedy właściwośc będzie dostępna tylko z wnętrza projektu
+        {
+            // get, set - akcesory plus logika do nicch:
+            get // jak usuniemy get będziemy mogli tylko zapisywać poprzez set (włąściwość tylko do zapisu)| można rónież zrobić odwrotnie (właściwość tylko do odczytu)
+            {
+                // tutaj możemy dodać dowolny kod
+                return _nazwisko;
+            }
+            set // ustawia wartość _nazwisko na value
+            {
+                // tutaj możemy dodać dowolny kod
+                _nazwisko = value;
+            }
+        }
+            // jest też możliwość ustawienia właściwości automatycznej - tutaj właściwość Imię:
+
+            public string Imie { get; set; }
+        // w obecnej sytuacji zapisy są równoważne - > brak dodatkowego kodu
+        //skrót do utworzenia właściwości to prop + tab tab
+        public string Email { get; set; }
+
+        public int KlientId { get; private set; } // setter prywatny, nie będzie dostępu zewnątrz
+
+        public string ImieNazwisko 
+        {
+            get 
+            {
+                return Imie + "," + Nazwisko;
+            } 
+        }
+
+
+
+
+
+
+
+
+    }
+}
+}
